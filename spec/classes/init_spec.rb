8d57c46bf90b175a8da8d61e6cfbdbab1167d2f7
Joe Topjian <joe@topjian.net>
2014-10-12 01:55:42 +0000

    Updating spec test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -47,4 +47,44 @@ describe 'reprepro' do
 
   end
 
+  context "With non-default parameters" do
+    let :params do
+      {
+        :basedir => '/home/packages',
+        :homedir => '/home/packages',
+      }
+    end
+
+    it { should contain_package('reprepro') }
+    it { should contain_group('reprepro').with_name('reprepro') }
+    it do
+      should contain_user('reprepro').with({
+        :name       => 'reprepro',
+        :home       => '/home/packages',
+        :shell      => '/bin/bash',
+        :comment    => 'Reprepro user',
+        :gid        => 'reprepro',
+        :managehome => true,
+      }).that_requires('Group[reprepro]')
+    end
+
+    it do
+      should contain_file('/home/packages/bin').with({
+        :ensure => 'directory',
+        :mode   => '0755',
+        :owner  => 'reprepro',
+        :group  => 'reprepro',
+      }).that_requires('User[reprepro]')
+      end
+
+    it do
+      should contain_file('/home/packages/bin/update-distribution.sh').with({
+        :mode    => '0755',
+        :content => /while getopts/,
+        :owner   => 'reprepro',
+        :group   => 'reprepro',
+      }).that_requires('File[/home/packages/bin]')
+    end
+
+  end
 end
